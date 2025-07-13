cffb0f19ef56ae066b69f01bbfc9e40ed5d59a76
SERDUN <dmitriiserdun@gmail.com>
2025-07-13 22:34:21 +0300

    fix: add correct rationg validation

--- src/features/tea/dto/tea.dto.ts
+++ src/features/tea/dto/tea.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const TeaSchema = z.object({
     id: z.string(),
     name: z.string().min(2).max(30),
-    rating: z.number().min(1).max(5).optional(),
+    rating: z.number().min(1).max(10).optional(),
     brewTemp: z.number().min(60).max(100).optional(),
     notes: z.string().max(20).optional(),
 });
