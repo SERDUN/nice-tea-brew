51ab24d92f8efabf944f8386181b356158174914
SERDUN <dmitriiserdun@gmail.com>
2025-07-13 21:25:57 +0300

    feat: add onApplicationShutdown signal message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+export class AppService implements OnApplicationShutdown {
+    onApplicationShutdown(signal: string) {
+        if (signal === 'SIGINT') {
+            console.log('Bye tea-lovers');
+        }
+    }
 }
